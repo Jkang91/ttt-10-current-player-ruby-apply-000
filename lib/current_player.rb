803995048370a2d9a617fb613a2ac5691706115c
June Young Kang <jkang34@gmail.com>
2020-09-24 03:04:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,9 @@ def turn_count(board)
   counter = 0 
   board.each do |a|
     if a == "X" || "O"
-      counter += 1 
+      counter += 1
+    else
+      turn_count(board)
     end
   end
   counter
